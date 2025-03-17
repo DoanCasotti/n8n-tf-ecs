d284f97df24a5e409d2940d45de9190befb2992b
alisrios <alisrios@gmail.com>
2025-03-16 21:24:27 -0300

    Adiconado rds, cluster, sg, ecs role

---
+++ iam.ecs.role.tf
@@ -0,0 +1,63 @@
+# Criando a IAM Role para instâncias ECS
+resource "aws_iam_role" "ecs_instance_role" {
+  name = "ecs-instance-role-tf"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+# Anexando políticas gerenciadas à Role
+resource "aws_iam_role_policy_attachment" "ecs_instance_role_policy" {
+  role       = aws_iam_role.ecs_instance_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
+}
+
+resource "aws_iam_role_policy_attachment" "ssm_managed_instance_core" {
+  role       = aws_iam_role.ecs_instance_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+# Criando o Instance Profile para ser usado pelas instâncias do ECS
+resource "aws_iam_instance_profile" "ecs_node" {
+  name = "ecs-instance-role-profile-tf"
+  path        = "/ecs/instance/"
+  role        = aws_iam_role.ecs_instance_role.name
+}
+
+# Criando a IAM Role para execução de tarefas no ECS
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "ecs-task-execution-role-tf"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ecs-tasks.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+# Anexando a política gerenciada AmazonECSTaskExecutionRolePolicy
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
