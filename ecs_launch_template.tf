b271d79475c9854f41ce36d3339410c5923aa91c
alisrios <alisrios@gmail.com>
2025-03-17 22:26:24 -0300

    Adicionado Service redis

--- ecs_launch_template.tf
+++ ecs_launch_template.tf
@@ -9,6 +9,14 @@ resource "aws_launch_template" "ecs_ec2" {
   user_data = base64encode(<<-EOF
       #!/bin/bash
       echo ECS_CLUSTER=${aws_ecs_cluster.this.name} >> /etc/ecs/ecs.config;
+      
+      echo '#!/bin/bash' > /etc/rc.d/init.d/set_sysctl
+      echo 'sysctl -w vm.overcommit_memory=1' >> /etc/rc.d/init.d/set_sysctl
+      chmod +x /etc/rc.d/init.d/set_sysctl
+      
+      chkconfig --add set_sysctl
+      service set_sysctl start
+
     EOF
   )
 }
