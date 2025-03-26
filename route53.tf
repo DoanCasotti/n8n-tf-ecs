e7d01ab4a1b3382b9adf3fd0d264376ae4f78928
alisrios <alisrios@gmail.com>
2025-03-26 11:21:15 -0300

    Ajuste de services, certificado, tasks, state config, alb, route53, variables

--- route53.tf
+++ route53.tf
@@ -1,10 +1,10 @@
 data "aws_route53_zone" "primary" {
-  name = "hardcloud.com.br"
+  name = "jovando.com.br"
 }
 
 resource "aws_route53_record" "n8n10" {
   zone_id = data.aws_route53_zone.primary.zone_id
-  name    = "n8n10.hardcloud.com.br"
+  name    = "n8n10.jovando.com.br"
   type    = "CNAME"
   ttl     = 300
   records = [aws_lb.this.dns_name]
